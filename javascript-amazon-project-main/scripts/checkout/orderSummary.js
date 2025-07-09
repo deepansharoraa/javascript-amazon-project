b6fc25769439a55c78313bd1accc4167d958caed
deepansharoraa <deepansharora14@gmail.com>
2025-07-09 14:15:16 +0530

    Create automated tests for order summary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -9,13 +9,6 @@ import { renderPaymentSummary } from './paymentSummary.js';
 
 
 
-
-
-
-
-
-
-
 export function renderOrderSummary(){
 
 
@@ -50,6 +43,7 @@ const today = dayjs();
 
   cartSummaryHTML += `
     <div class="cart-item-container
+      js-cart-item-container 
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
@@ -66,14 +60,14 @@ const today = dayjs();
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
