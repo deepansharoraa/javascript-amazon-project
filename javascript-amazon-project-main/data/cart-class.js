727006bbf69796828d0247d0ff865bbedc782887
deepansharoraa <deepansharora14@gmail.com>
2025-07-10 12:28:35 +0530

    Use inheritance and polymorphism to add size chart

--- javascript-amazon-project-main/data/cart-class.js
+++ javascript-amazon-project-main/data/cart-class.js
@@ -1,14 +1,13 @@
-class Cart{ 
-    cartItems;
-    #localStorageKey;
+class Cart {
+  cartItems;
+  #localStorageKey;
 
-    constructor(localStorageKey){
-        this.#localStorageKey = localStorageKey;
-        this.#loadFromStorage();
-    }
+  constructor(localStorageKey) {
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
+  }
 
-
-    #loadFromStorage(){
+  #loadFromStorage() {
     this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
@@ -20,87 +19,68 @@ class Cart{
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
         quantity: 1,
         deliveryOptionId: '2'
-
       }];
     }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: '1'
+      });
     }
 
+    this.saveToStorage();
+  }
 
+  removeFromCart(productId) {
+    const newCart = [];
 
-    saveToStorage() {
-        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
-    }
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
 
+    this.cartItems = newCart;
 
+    this.saveToStorage();
+  }
 
-    addToCart(productId) {
-        let matchingItem;
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
 
-        this.cartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-            }
-        });
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
 
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            this.cartItems.push({
-            productId: productId,
-            quantity: 1,
-            deliveryOptionId: '1'
-            });
-        }
+    matchingItem.deliveryOptionId = deliveryOptionId;
 
-        this.saveToStorage();
-        }
-
-        removeFromCart(productId) {
-        const newCart = [];
-
-        this.cartItems.forEach((cartItem) => {
-            if (cartItem.productId !== productId) {
-            newCart.push(cartItem);
-            }
-        });
-
-        this.cartItems = newCart;
-
-        this.saveToStorage();
-        }
-
-
-        updateDeliveryOption(productId, deliveryOptionId){
-        let matchingItem;
-
-        this.cartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-            }
-        });
-
-
-        matchingItem.deliveryOptionId = deliveryOptionId;
-        this.saveToStorage();
-        }
-
-
+    this.saveToStorage();
+  }
 }
-
 
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
-
-
-
-
 console.log(cart);
 console.log(businessCart);
-
-
-
-
-
-
-
+console.log(businessCart instanceof Cart);
